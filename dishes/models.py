280cb3c8c0a083f7cb4224c1d256bac5c5dd6375
lesnik512 <lesnik512@gmail.com>
2017-06-08 12:08:03 +0500

    models for dishes app created

--- dishes/models.py
+++ dishes/models.py
@@ -4,3 +4,26 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    weight = models.FloatField()
+    calorie = models.PositiveIntegerField()
+    price = models.PositiveIntegerField()
+    TYPES_OF_DISHES = (
+        ('HOT', 'hot'),
+        ('SECOND', 'second'),
+        ('SALAD', 'salad'),
+        ('DESSERT', 'dessert'),
+    )
+    type = models.CharField(max_length=10,
+                            choices=TYPES_OF_DISHES,
+                            default='HOT')
+
+class Dinner(models.Model):
+    name = models.CharField(max_length=255)
+    dishes = models.ForeignKey(Dish)
+    image = models.FileField(upload_to='uploads/')
+
+class DailyMenu(models.Model):
+    date = models.DateField(auto_now=True)
+    dinners = models.ForeignKey(Dinner)
