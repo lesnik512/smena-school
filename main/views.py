5e5b71964adeb7159ebab45528b12d7ce789cdb5
lesnik <lesnik@gmail.com>
2017-07-18 15:41:50 +0500

    trash commit

--- main/views.py
+++ main/views.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-fdh
 from datetime import timedelta, date, time, datetime
 
 from django.contrib.auth import login, authenticate, logout
